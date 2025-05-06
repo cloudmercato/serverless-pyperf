0e76b0ac20f66a83b6a2c42457537b3d6a1dfd1f
Anthony Monthe <anthony.monthe@gmail.com>
2025-05-06 23:04:55 +0200

    Added Azure functions

---
+++ azure-functions/function_app.py
@@ -0,0 +1,41 @@
+import json
+import azure.functions as func
+from serverless_pyperf import run, mercato
+
+app = func.FunctionApp()
+
+
+@app.route(route="pyperf", auth_level=func.AuthLevel.ANONYMOUS)
+def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
+    options = req.get_json()
+    benchmark_names = [b.name for b in run.BENCHMARKS]
+    if options.get('benchmarks'):
+        benchmark_names = [
+            b for b in benchmark_names
+            if b in options['benchmarks']
+        ]
+
+    suite, errors = run.run(
+        benchmark_names=benchmark_names,
+        same_loops=None,
+        inherit_environ=None,
+        debug_single_value=False,
+        rigorous=options.get('rigorous', False),
+        fast=options.get('fast', False),
+        verbose=options.get('verbose', False),
+        affinity=options.get('affinity'),
+        track_memory=options.get('track_memory'),
+        min_time=options.get('min_time'),
+        timeout=options.get('timeout'),
+        hook=[],
+    )
+
+    if errors:
+        print(errors)
+    mercato.upload(suite, errors)
+
+    results = run.parse_results(suite, errors)
+    return func.HttpResponse(
+        json.dumps(results),
+        status_code=200,
+    )
