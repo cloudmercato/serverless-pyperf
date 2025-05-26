4f61189970cd91f656c8f0f1ad70cd541a352079
Anthony Monthe <anthony.monthe@gmail.com>
2025-05-26 13:10:42 +0200

    Update __main__

--- serverless_pyperf/__main__.py
+++ serverless_pyperf/__main__.py
@@ -1,8 +1,2 @@
 #!/usr/bin/env python
-from . import run
-
-
-def main(*args, **kwargs):
-    suite, errors = run.run(*args, **kwargs)
-    results = run.parse_results(suite, errors)
-    print(results)
+from .main import main
